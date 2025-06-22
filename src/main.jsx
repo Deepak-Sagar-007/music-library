3cb86b4ebec2ab439f4c90eac2a7c0e1aca2704a
Deepak Sagar <deepaksagar924@gmail.com>
2025-06-22 14:02:15 +0530

    Configured build target for Netlify deployment

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import MusicLibrary from './MusicLibrary.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    
+<MusicLibrary/>
   </StrictMode>,
 )
